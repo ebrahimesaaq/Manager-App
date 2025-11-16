a8424a206c5f23921ee8839d4546752344806a7e
Ebrahim <ebrahimesaaq@gmail.com>
2025-11-16 18:27:15 +0200

    Add project to git

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const ManagerApp());
+}
+class ManagerApp extends StatelessWidget {
+  const ManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
